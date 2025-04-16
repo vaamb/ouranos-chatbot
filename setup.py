5a62d6fd162add9ef183645ba801dfe81a425865
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-04-16 21:27:11 +0200

    Use `pyproject.toml`

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    entry_points={
-        "ouranos.plugins":
-            ["chatbot=ouranos_chatbot.plugin_setup:plugin"]
-    },
-)
--- src/ouranos_chatbot/__init__.py
+++ src/ouranos_chatbot/__init__.py
@@ -1,1 +1,3 @@
+__version__ = "0.1.0"
+
 from ouranos_chatbot.main import Chatbot, main
