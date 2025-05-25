7e20f74f973eb81800420cb990aed8b329371f28
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-05-25 19:45:12 +0200

    Use `Ouranos`'s new `Plugin` system (#4)

--- src/ouranos_chatbot/main.py
+++ src/ouranos_chatbot/main.py
@@ -1,60 +1,26 @@
-import typing as t
-
-import click
 from telegram.ext import (
     Application, ApplicationBuilder, CommandHandler, filters, MessageHandler)
 
 from gaia_validators import missing
-from ouranos import current_app
-from ouranos.sdk import Functionality, run_functionality_forever
+from ouranos.core.config import ConfigDict
+from ouranos.sdk import Functionality
 
 from ouranos_chatbot.config import Config
 
 
-if t.TYPE_CHECKING:
-    from ouranos.core.config import profile_type
-
-
-@click.command()
-@click.option(
-    "--config-profile",
-    type=str,
-    default=None,
-    help="Configuration profile to use as defined in config.py.",
-    show_default=True,
-)
-def main(
-        config_profile: str,
-) -> None:
-    """Launch Ouranos'Chatbot
-
-    The Chatbot provides a communication mean between Ouranos and the user
-    using telegram. It provides commands that allows the user to get data from
-    the database and manage Gaia's instances
-    """
-    run_functionality_forever(Chatbot, config_profile)
-
-
 class Chatbot(Functionality):
-    def __init__(
-            self,
-            config_profile: "profile_type" = None,
-            config_override: dict | None = None,
-            **kwargs
-    ) -> None:
-        super().__init__(config_profile, config_override, **kwargs)
-        token = current_app.config.get("TELEGRAM_BOT_TOKEN", missing)
+    def __init__(self, config: ConfigDict, **kwargs) -> None:
+        super().__init__(config, **kwargs)
+        token = self.config.get("TELEGRAM_BOT_TOKEN", missing)
         if token is missing:
             self.logger.warning(
                 "The config class used for Ouranos does not subclass "
-                "`ouranos_chatbot.Config`. Falling back to default values."
-            )
+                "`ouranos_chatbot.Config`. Falling back to default values.")
             token = Config().TELEGRAM_BOT_TOKEN
         if token is None:
             self.logger.error(
                 "The config parameters 'TELEGRAM_BOT_TOKEN' is not set, it is"
-                "not possible to use the chatbot functionality"
-            )
+                "not possible to use the chatbot functionality.")
         self.token = token
         self.application: Application | None = None
 
@@ -85,7 +51,6 @@ class Chatbot(Functionality):
         await self.application.initialize()
         await self.application.updater.start_polling()
         await self.application.start()
-
 
     async def _shutdown(self):
         if self.application.updater.running:
