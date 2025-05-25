399b4fecdd538f9bbdd27d9584411db6a46fa941
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-05-25 20:05:17 +0200

    Add a TTL cache to `get_current_user`

--- src/ouranos_chatbot/auth.py
+++ src/ouranos_chatbot/auth.py
@@ -1,13 +1,23 @@
+from cachetools import TTLCache
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ouranos.core.database.models.app import anonymous_user, User, UserMixin
 
 
+cache_users = TTLCache(maxsize=32, ttl=60)
+
+
 async def get_current_user(session: AsyncSession, telegram_id: int) -> UserMixin:
-    user = await User.get_by(session, telegram_id=telegram_id)
-    if user:
-        return user
-    return anonymous_user
+    try:
+        return cache_users[telegram_id]
+    except KeyError:
+        user = await User.get_by(session, telegram_id=telegram_id)
+        if user:
+            cache_users[telegram_id] = user
+            session.expunge(user)
+            session.expunge(user.role)
+            return user
+        return anonymous_user
 
 
 async def link_user(
